747559e525ed07306fa470cb4edc10599c9c6dc9
Dylan Drumm <dyl.drumm@gmail.com>
2023-10-29 17:05:01 -0400

    hjk

--- test-app/src/components/nav/Navbar.js
+++ test-app/src/components/nav/Navbar.js
@@ -7,7 +7,21 @@ class Navbar extends Component {
     render() {
         return(
             <nav className = "Navbar">
-                
+                <h1>
+                    Test Site
+                </h1>
+                <ul className="NavbarItems">
+                    {NavbarItems.map((item, index) => {
+                        return(
+                            <li key={index}>
+                                <a href={item.url} className={item.cName}>
+                                    <i className={item.icon}></i>
+                                    {item.title}
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
             </nav>
         );
     }
--- test-app/src/components/nav/NavbarItems.js
+++ test-app/src/components/nav/NavbarItems.js
@@ -1,3 +1,8 @@
 export const NavbarItems = [
-    
+    {
+        title: " Profile",
+        url: "#",
+        cName:"nav-links",
+        icon:"fa-solid fa-user-alt  "
+    },
 ]
